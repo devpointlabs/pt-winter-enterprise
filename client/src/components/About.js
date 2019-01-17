d7ebc8c2d6f5f2bd5189d4672c20313409a97887
Harrison Pearce <jhpearce77@gmail.com>
2019-01-16 20:44:21 -0700

    fixed typos in res controller and about.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,21 +3,23 @@ import {Card, Header, Image} from 'semantic-ui-react';
 import {BrowserRouter} from 'react-router-dom';
 
 const About = () => (
-    <Header>About Enterprise International</Header>
-    <Card>
-         <Card.Content color= 'white'>
-            <p>
-                Bacon ipsum dolor amet filet mignon burgdoggen spare ribs buffalo, strip steak ribeye picanha meatball rump tri-tip fatback. Chicken cow pork chop landjaeger ball tip pork belly short ribs boudin porchetta tail t-bone hamburger cupim. Burgdoggen jowl alcatra capicola sausage chuck. Drumstick venison burgdoggen filet mignon doner. Pork belly t-bone pork loin, cow pork ham hamburger shankle fatback turkey. Short ribs tongue venison, shoulder spare ribs meatball pork belly burgdoggen sirloin ball tip beef ribs cow. Beef ribs shank pastrami ribeye.
+    <div>
+        <Header>About Enterprise International</Header>
+        <Card>
+            <Card.Content color= 'white'>
+                <p>
+                    Bacon ipsum dolor amet filet mignon burgdoggen spare ribs buffalo, strip steak ribeye picanha meatball rump tri-tip fatback. Chicken cow pork chop landjaeger ball tip pork belly short ribs boudin porchetta tail t-bone hamburger cupim. Burgdoggen jowl alcatra capicola sausage chuck. Drumstick venison burgdoggen filet mignon doner. Pork belly t-bone pork loin, cow pork ham hamburger shankle fatback turkey. Short ribs tongue venison, shoulder spare ribs meatball pork belly burgdoggen sirloin ball tip beef ribs cow. Beef ribs shank pastrami ribeye.
 
-                 Salami shankle pork capicola biltong, boudin chuck rump tongue turducken turkey cupim. Turkey chicken tail short ribs buffalo chuck. Meatball cupim ribeye alcatra pork loin beef ribs prosciutto leberkas. Flank porchetta corned beef turkey t-bone bacon salami meatball.
+                    Salami shankle pork capicola biltong, boudin chuck rump tongue turducken turkey cupim. Turkey chicken tail short ribs buffalo chuck. Meatball cupim ribeye alcatra pork loin beef ribs prosciutto leberkas. Flank porchetta corned beef turkey t-bone bacon salami meatball.
 
-            </p>
-        
-        </Card.Content>
-    </Card>
-    <Image src=''/>
-    <Image src=''/>
-    <Image src=''/>
+                </p>
+            
+            </Card.Content>
+        </Card>
+        <Image src=''/>
+        <Image src=''/>
+        <Image src=''/>
+    </div>
 
 )
   
